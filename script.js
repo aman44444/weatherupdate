cad74d5881d0a0e7f216016b494bee6d463ca4e0
aman1357 <amancorp04@gmail.com>
2024-06-08 23:09:27 +0530

    .

--- script.js
+++ script.js
@@ -1,46 +1,47 @@
-const btn = document.getElementById('btn');
+document.getElementById('btn').addEventListener('click', getWeather);
 
-const bodyImg =document.getElementById('body');
-    const input = document.getElementById('city-name');
+function getWeather() {
+    const cityName = document.getElementById('city-name').value;
+    const apiKey = 'b0a9e830f5f0cb0837b4f95219568274';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
 
-// btn.addEventListener('click', temp);
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data =>{ 
+          console.log(data);
+          updateWeather(data)}
+        )
+        .catch(error => console.error("Error:", error));
+}
 
- function temp(){
-     console.log('click')
-     const bodyImg = document.getElementById('body');
-    const input = document.getElementById('city-name');
-     const img = document.getElementById('img');
-     fetch('https://api.openweathermap.org/data/2.5/weather?q='+ input.value +'&units=imperial&appid=b0a9e830f5f0cb0837b4f95219568274').then(response => response.json()).then(data => {
-   console.log(data)
-    const Kel = data.main.temp;
-    const cel = Math.floor(((Kel-32)*5/9));
-    document.getElementById('temp').innerHTML = cel;
-     document.getElementById('humi').innerHTML = data.main.humidity+`%`;
-     document.getElementById('wind').innerHTML = data.wind.speed+`Km/h`;
-     
-     const weather = document.getElementById('condition');
-     const weatherData = data.weather[0].main;
-       weather.innerHTML = weatherData;
-     
-      if(weatherData.match('Mist')){
-        
-        img.src="/imgs/mist.gif"
-      }if(weatherData.match('Rain')){
-      
-       img.src="/imgs/raining-rainy-weather.gif"
-      }if(weatherData.match('Clouds')){
-       
-        img.src="/imgs/cloudy.gif";
-       }if(weatherData.match('Clear')){
-       
-        img.src="/imgs/sunny.gif";
-       }if(weatherData.match('Drizzle')){
-      
-      img.src="/imgs/mist.gif"
-      }
-      
-    }).catch(error => console.log("errao"));   
-  
-  input.value='';
-   
+function updateWeather(data) {
+    const tempElement = document.getElementById('temp');
+    const humidityElement = document.getElementById('humi');
+    const windElement = document.getElementById('wind');
+    const conditionElement = document.getElementById('condition');
+    const imgElement = document.getElementById('img');
+
+    const temperature = Math.round(data.main.temp);
+    const humidity = data.main.humidity;
+    const windSpeed = data.wind.speed;
+    const weatherCondition = data.weather[0].main;
+
+    tempElement.innerHTML = temperature;
+    humidityElement.innerHTML = `${humidity}%`;
+    windElement.innerHTML = `${windSpeed} Km/h`;
+    conditionElement.innerHTML = weatherCondition;
+
+    const weatherImages = {
+        Mist: "imgs/mist.gif",
+        Rain: "imgs/raining-rainy-weather.gif",
+        Clouds: "imgs/cloudy.gif",
+        Clear: "imgs/sunny.gif",
+        Drizzle: "imgs/mist.gif",
+        Thunderstorm : "imgs/thunderstorm.gif",
+        Snow:"imgs/snow-winter.gif",
+        Haze:"imgs/haze.gif"
+    };
+
+    imgElement.src = weatherImages[weatherCondition] || "imgs/defaultimg.jpg";
+    document.getElementById('city-name').value = '';
 }
